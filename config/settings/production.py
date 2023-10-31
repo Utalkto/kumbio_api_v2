d8e96b6144f90edb4ecf3de354dae92d090836ea
Yesid9126 <yesidf26@gmail.com>
2023-10-31 10:49:56 -0500

    add CSRF_TRUSTED_ORIGINS

--- config/settings/production.py
+++ config/settings/production.py
@@ -15,6 +15,9 @@ from .base import env
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["dev2.api.kumbio.com"])
+CSRF_TRUSTED_ORIGINS = [
+    "https://dev2.api.kumbio.com",
+]
 
 # DATABASES
 # ------------------------------------------------------------------------------
