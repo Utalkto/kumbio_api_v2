1739e74836768d0da01abb13c4a1800727ef6c62
Yesid9126 <yesidf26@gmail.com>
2023-11-21 10:52:24 -0500

    profile model

--- kumbio_api_v2/users/admin.py
+++ kumbio_api_v2/users/admin.py
@@ -6,6 +6,15 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+# Models
+from kumbio_api_v2.users.models import Profile
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    extra = 0
+
+
 @admin.register(User)
 class UserAdmin(auth_admin.UserAdmin):
     fieldsets = [
@@ -46,3 +55,4 @@ class UserAdmin(auth_admin.UserAdmin):
             },
         ),
     )
+    inlines = [ProfileInline]
