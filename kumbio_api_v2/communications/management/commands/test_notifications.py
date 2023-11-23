732b63e18e2355f31764fdce8784d1fc1e75c1d6
Emilio Gerdez <emiliojgerdezd@gmail.com>
2023-11-22 20:17:51 -0400

    agregadas comunicaciones

--- kumbio_api_v2/communications/management/commands/test_notifications.py
+++ kumbio_api_v2/communications/management/commands/test_notifications.py
@@ -1,19 +1,18 @@
 from cgi import test
 from unittest.mock import patch
-from communications.notifications import send_whatsapp
-from communications.notifications import send_email
+
+from communications.notifications import send_email, send_whatsapp
 
 # Django
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
-
     def handle(self, *args, **options):
+        send_whatsapp("+584124781318", "Hello, World! prueba realizada correctamente") == True
+        send_whatsapp("+584123058807", "Hello, World! prueba realizada correctamente") == False
+        send_whatsapp("+573126212123", "Hello, World! prueba realizada correctamente") == False
+        send_whatsapp("+14034012088", "Hello, World! prueba realizada correctamente") == False
 
-            send_whatsapp("+584124781318", "Hello, World! prueba realizada correctamente") == True
-            send_whatsapp("+584123058807", "Hello, World! prueba realizada correctamente") == False
-            send_whatsapp("+573126212123", "Hello, World! prueba realizada correctamente") == False
-            send_whatsapp("+14034012088", "Hello, World! prueba realizada correctamente") == False
-
-            send_email("emiliojgerdezd@gmail.com", "Test Subject", "<p>Test Body</p>") == True
-            send_email("emiliojgerdezd@gmail.com", "Test Subject", "<p>Test Body</p>") == False
+        send_email("emiliojgerdezd@gmail.com", "Test Subject", "<p>Test Body</p>") == True
+        send_email("emiliojgerdezd@gmail.com", "Test Subject", "<p>Test Body</p>") == False
