ad2426f1ca504dbb9d106e3da5bf926383c77a7b
Yesid9126 <yesidf26@gmail.com>
2023-12-27 07:55:00 -0500

    refactor

--- kumbio_api_v2/appointments/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from kumbio_api_v2.appointments.api.views import ProfessionalAvailability
-
-app_name = "appointments"
-urlpatterns = [
-    path("professional-availability/", ProfessionalAvailability.as_view(), name="professional-availability"),
-]
